cbbedf3b67e23ac0eb446bb54ebcb2a0fc6b9625
johnpaolosantos <johnpaolosantos527@gmail.com>
2026-06-17 09:15:29 +0800

    add docker for deployment, express server static file serving

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,38 @@
-import express from "express";        
+import express from "express";
 import "dotenv/config";
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
-import { clerkMiddleware } from '@clerk/express'
-import cors from 'cors';
+import { clerkMiddleware } from "@clerk/express";
+import cors from "cors";
+
+import fs from "fs";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
+const publicDir = path.join(process.cwd(), "public");
+
 app.use(express.json());
-app.use(cors({origin: FRONTEND_URL, credentials: true}));
-app.use(clerkMiddleware())
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
+app.use(clerkMiddleware());
 
 app.get("/health", (req, res) => {
-    res.status(200).json({ok: true});
-})
+    res.status(200).json({ ok: true });
+});
+
+// if the public directory exists, serve the static files
+// this is for the production build
+if (fs.existsSync(publicDir)) {
+    app.use(express.static(publicDir));
+    app.get("/{*any}", (req, res, next) => {
+        res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+    });
+}
 
 app.listen(PORT, () => {
     connectDB();
-    console.log("Server is up and running on port: ", PORT)
+    console.log("Server is up and running on port: ", PORT);
+    // if (process.env.NODE_ENV === "production") job.start();
 });
