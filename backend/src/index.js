3f4be6e2f13023c191b9f3b5f4b59bda92c5d8cb
johnpaolosantos <johnpaolosantos527@gmail.com>
2026-06-17 08:09:34 +0800

    add db,schema & middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,23 @@
 import express from "express";        
 import "dotenv/config";
+import User from "./models/user.model.js";
+import { connectDB } from "./lib/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
-app.listen(PORT, () => console.log("Server is up and running on port: ", PORT));
+app.use(express.json());
+app.use(cors({origin: FRONTEND_URL, credentials: true}));
+app.use(clerkMiddleware())
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ok: true});
+})
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log("Server is up and running on port: ", PORT)
+});
