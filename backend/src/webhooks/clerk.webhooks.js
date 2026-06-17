1ec0c17fb8aca71deb03a0efd13607f03467cd45
johnpaolosantos <johnpaolosantos527@gmail.com>
2026-06-17 09:56:15 +0800

    fix

--- backend/src/webhooks/clerk.webhooks.js
+++ backend/src/webhooks/clerk.webhooks.js
@@ -1,6 +1,6 @@
 import express from "express";
 import User from "../models/user.model.js";
-import { verifyWebhook } from "@clerk/backend/webhooks";
+import { verifyWebhook } from "@clerk/backend/webhooks.js";
 
 const router = express.Router();
 
