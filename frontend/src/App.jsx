74533bdd115a494fec6c74efac2ae96285d30dcb
johnpaolosantos <johnpaolosantos527@gmail.com>
2026-06-17 13:37:24 +0800

    frontend auth page setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,42 @@
-import "./App.css";
-import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
+import { WallpaperProvider } from "./context/WallpaperContext";
+import { ThemeProvider } from "./context/ThemeContext";
+
+import { Routes, Route, Navigate } from "react-router";
+import ChatPage from "./pages/ChatPage.jsx";
+import AuthPage from "./pages/AuthPage.jsx";
+import { useAuth } from "@clerk/react";
 
 function App() {
-  return (
-    <div>
-      <h1>PaoMessage</h1>
+    const { isSignedIn, isLoaded } = useAuth();
 
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider>
+            <WallpaperProvider>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            isSignedIn ? (
+                                <ChatPage />
+                            ) : (
+                                <Navigate to="/auth" replace />
+                            )
+                        }
+                    ></Route>
+                    <Route
+                        path="/auth"
+                        element={
+                            !isSignedIn ? (
+                                <AuthPage />
+                            ) : (
+                                <Navigate to="/auth" replace />
+                            )
+                        }
+                    ></Route>
+                </Routes>
+            </WallpaperProvider>
+        </ThemeProvider>
+    );
 }
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ClerkProvider } from '@clerk/react';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ClerkProvider } from "@clerk/react";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ClerkProvider>
-        <App />
-    </ClerkProvider>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <ClerkProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ClerkProvider>
+    </StrictMode>,
+);
